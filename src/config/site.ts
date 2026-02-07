189a0ffafe4136f13f84525c8d7274ef2d9ef663
Rafael Cenzano <32753063+RafaelCenzano@users.noreply.github.com>
2026-02-07 01:16:26 -0500

    feat: add site config

---
+++ src/config/site.ts
@@ -0,0 +1,7 @@
+export const SITE = {
+  name: "Epsilon Zeta Alumni Association",
+  url: "https://apoezaa.org",
+  description: "The Epsilon Zeta Alumni Association hosts events and anniversaries for alumni of Epsilon Zeta Chapter of Alpha Phi Omega",
+  defaultImage: "/favicon.png",
+} as const;
+
